7cedefef6f2227a22ecbd03c6ef9106ce4f880fa
techwolf12 <christiaan@droiddev.nl>
2014-12-26 16:30:56 +0100

    Setup + ssh

---
+++ setup.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+
+BACKUP_DIR=$PWD/rc_backup
+
+ORIGINAL_BASHRC=$HOME/.bashrc
+ORIGINAL_ZSHRC=$HOME/.zshrc
+ORIGINAL_DIRCOLORS=$HOME/.dircolors
+ORIGINAL_PROFILE=$HOME/.profile
+ORIGINAL_GITCONFIG=$HOME/.gitconfig
+ORIGINAL_VIMRC=$HOME/.vimrc
+ORIGINAL_GNOME_TERMINAL=$HOME/.gconf/apps/gnome-terminal
+ORIGINAL_FONTS=$HOME/.fonts
+PACKAGES=(
+    vim git tig tree htop synapse zsh google-chrome-stable gparted audacity powertop zip xclip vlc valgrind unrar unzip ipython python3 qalculate openssh-server keepass2 imagemagick lxappearance compizconfig-settings-manager pipelight-multi dropbox oracle-java8-installer google-talkplugin shutter skype nano keepassx ctags python3-numpy nmap python-appindicator ntfs-3g
+)
+
+#Move all original rc files to a backup dir.
+mkdir -p $BACKUP_DIR
+mv \
+$ORIGINAL_BASHRC \
+$ORIGINAL_ZSHRC \
+$ORIGINAL_DIRCOLORS \
+$ORIGINAL_PROFILE \
+$ORIGINAL_GITCONFIG \
+$ORIGINAL_VIMRC \
+$ORIGINAL_GNOME_TERMINAL \
+$ORIGINAL_FONTS \
+$BACKUP_DIR
+
+# Setup Symlinks for all files
+ln -sf $PWD/bash/bashrc $ORIGINAL_BASHRC
+ln -sf $PWD/zsh/zshrc $ORIGINAL_ZSHRC
+ln -sf $PWD/bash/dircolors $ORIGINAL_DIRCOLORS
+ln -sf $PWD/bash/profile $ORIGINAL_PROFILE
+ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONFIG
+ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
+ln -sf $PWD/gnome-terminal $ORIGINAL_GNOME_TERMINAL
+ln -sf $PWD/fonts $ORIGINAL_FONTS
+
+# Install the newest updates for already installed packages
+sudo apt-get --yes --force-yes upgrade
+
+sudo dpkg --add-architecture i386
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+if [[ ! -e "/etc/apt/sources.list.d/google-chrome.list" ]];
+then
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+fi
+
+if [[ ! -e "/etc/apt/sources.list.d/google-talkplugin.list" ]];
+then
+    sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google-talkplugin.list'
+fi
+
+# Skype
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+# Java
+sudo add-apt-repository ppa:webupd8team/java
+
+# Pipeline
+sudo apt-add-repository ppa:pipelight/stable
+
+# Dropbox
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+if [[ ! -e "/etc/apt/sources.list.d/dropbox.list" ]];
+then
+    sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ trusty main" >> /etc/apt/sources.list.d/dropbox.list'
+fi
+
+sudo apt-get update
+
+for i in "${PACKAGES[@]}"
+do
+    sudo apt-get --yes --force-yes install $i
+done
+
+sudo pipelight-plugin --enable silverlight
+
+# Zsh
+chsh -s /bin/zsh
+
+# SSH keys
+mkdir $HOME/.ssh
+gpg --output $HOME/.ssh/id_ecdsa --decrypt $PWD/ssh/id_ecdsa.gpg
+gpg --output $HOME/.ssh/id_rsa --decrypt $PWD/ssh/id_rsa.gpg
+chmod 600 $HOME/.ssh/id_*
+cp $PWD/ssh/{config,id_rsa.pub,id_ecdsa.pub} $HOME/.ssh/
