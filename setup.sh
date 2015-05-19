734beabda575fb360507983d49827c49aff0576d
techwolf12 <christiaan@droiddev.nl>
2015-05-19 10:45:39 +0200

    Add stuff

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ ORIGINAL_FONTS=$HOME/.fonts
 ORIGINAL_GNUPGCONF=$HOME/.gnupg/gpg.conf
 ORIGINAL_MUTTRC=$HOME/.muttrc
 ORIGINAL_AWESOME=$HOME/.config/awesome
-ORIGINAL_IRSSI=$HOME/.irssi/irssi.conf
 PACKAGES=(
 vim irssi libreoffice git curl gawk firefox firefox-l10n-en-gb thunderbird thunderbird-l10n-en-gb tig tree htop build-essential cmake synapse scrot i3lock zsh newrelic-sysmond chromium-browser spotify-client gparted gnupg pcscd libccid audacity powertop zip xclip vlc valgrind unzip ipython python3 qalculate openssh-server imagemagick lxappearance pipelight-multi dropbox openjdk-7-jre openjdk-7-jdk shutter nano keepassx ctags python3-numpy nmap python-appindicator ntfs-3g awesome
 )
@@ -22,7 +21,6 @@ vim irssi libreoffice git curl gawk firefox firefox-l10n-en-gb thunderbird thund
 mkdir -p $BACKUP_DIR
 mv \
     $ORIGINAL_BASHRC \
-    $ORIGINAL_IRSSI \
     $ORIGINAL_ZSHRC \
     $ORIGINAL_DIRCOLORS \
     $ORIGINAL_PROFILE \
@@ -36,7 +34,7 @@ mv \
     $BACKUP_DIR
 
 # Ensures all directories are existing
-mkdir -p $HOME/.vim $HOME/.git $HOME/.gnupg $HOME/.irssi $HOME/.config/awesome
+mkdir -p $HOME/.vim $HOME/.git $HOME/.gnupg $HOME/.config/awesome
 
 # Scripts
 ln -sf $PWD/scripts $HOME
@@ -54,9 +52,6 @@ ln -sf $PWD/gnupg/gpg.conf $ORIGINAL_GNUPGCONF
 ln -sf $PWD/fonts $ORIGINAL_FONTS
 ln -sf $PWD/mutt/muttrc $ORIGINAL_MUTTRC
 ln -sf $PWD/awesome/ $ORIGINAL_AWESOME
-
-# Copy some files
-cp $PWD/irssi/irssi.conf $HOME/.irssi/config
 
 
 # Install the newest updates for already installed packages
